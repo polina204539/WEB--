2cc79cb76e6cd5cbcbdd450105ea345ea7ddbc24
polina204539 <90207185+polina204539@users.noreply.github.com>
2021-12-11 13:20:32 +0300

    Add files via upload

---
+++ kr/script.js
@@ -0,0 +1,41 @@
+const area = document.getElementById('area');
+let move = 0;
+let result = '';
+
+area.addEventListener('click', e => {
+  if(e.target.className = 'box') {
+    move % 2 == 0 ? e.target.innerHTML = 'X' : e.target.innerHTML = '0'; // определяет чей ход и что ставить
+    move++;
+    check()
+  }
+})
+
+const check = () => {
+  const boxes = document.getElementsByClassName('box');
+  const arr = [         
+      [0,1,2],  // выйгрыш по горизонтали
+      [3,4,5],  // выйгрыш по горизонтали
+      [6,7,8],  // выйгрыш по горизонтали
+      [0,3,6],  // выйгрыш по вертикали
+      [1,4,7],  // выйгрыш по вертикали
+      [2,5,8],  // выйгрыш по вертикали
+      [0,4,8],  // выйгрыш по горизонтали
+      [2,4,6],  // выйгрыш по горизонтали
+  ];
+  for(i = 0; i < arr.length; i++) {
+    if(
+      boxes[arr[i][0]].innerHTML == 'X' && boxes[arr[i][1]].innerHTML == 'X' && boxes[arr[i][2]].innerHTML == 'X' //проверка выигрышка X
+    ) {
+      result = 'крестики';
+      prepareResult(result);
+    } else if (
+        boxes[arr[i][0]].innerHTML == '0' && boxes[arr[i][1]].innerHTML == '0' && boxes[arr[i][2]].innerHTML == '0' //проверка выигрышка 0
+    ) {
+      result = 'нолики';
+      prepareResult(result);
+    }
+  }
+}
+const prepareResult = winner => {
+  console.log(winner);
+}
